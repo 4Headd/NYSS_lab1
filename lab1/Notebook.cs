d2f95d85716a2ba781dade68684876fe6175793b
User <User@DESKTOP-FAFS382>
2021-11-12 15:13:16 +0300

    Добавьте файлы проекта.

---
+++ lab1/Notebook.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace lab1
+{
+    class Notebook
+    {
+        private Dictionary<int, Person> notes;
+        private int id = 1;
+        public Notebook()
+        {
+            notes = new Dictionary<int, Person>();
+        }
+
+        public void AddNote(Person person)
+        {   
+            notes.Add(id, person);
+            ++id;
+        }
+
+
+        public bool EditNote(int id)
+        {
+            if (notes.ContainsKey(id))
+            {
+                Person person = new Person();
+                person.FillFields();
+                notes[id] = person;
+                return true;
+            }
+            return false;
+        }
+
+
+        public bool RemoveNote(int id)
+        {
+            if (notes.ContainsKey(id))
+            {
+                notes.Remove(id);
+                return true;
+            }
+            return false;
+        }
+
+        public Person Find(int id)
+        {
+            if (notes.ContainsKey(id))
+            {
+                
+                return notes[id];
+            }
+            return null;
+        }
+        
+        public void PrintAll()
+        {
+            foreach (var note in notes)
+            {
+                Console.WriteLine("#" + note.Key + " " + note.Value.Surname + " " + note.Value.Name + " " + note.Value.PhoneNumber);
+            }
+        }
+
+        public void ConsoleMenuInfo()
+        {
+            Console.WriteLine("Лист команд:");
+            Console.WriteLine("ADD, чтобы добавить запись в книжку");
+            Console.WriteLine("EDIT, чтобы редактировать существующую запись в книжке");
+            Console.WriteLine("DELETE, чтобы удалить существующую запись в книжке");
+            Console.WriteLine("FIND, чтобы вывести все существующие в книжке записи");
+            Console.WriteLine("PRINT, чтобы вывести краткую информацию о всех записях");
+            Console.WriteLine("INFO, чтобы вывести лист команд");
+            Console.WriteLine("END, чтобы выйти");
+        }
+
+        public void ConsoleMenu()
+        {
+            ConsoleMenuInfo();
+
+            Commands command;
+            do
+            {
+                Enum.TryParse(Console.ReadLine(), out command);
+                switch (command)
+                {
+                    case Commands.ADD:
+                        {
+                            Person person = new Person();
+                            person.FillFields();
+                            AddNote(person);
+                            break;
+                        }
+
+
+                    case Commands.EDIT:
+                        {
+                            Console.Write("Введите id записи, которую желаете изменить: ");
+                            int idToEdit = Convert.ToInt32(Console.ReadLine());
+
+                            if (EditNote(idToEdit))
+                            {
+                                Console.WriteLine($"Запись с id {idToEdit} изменена");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Запись с id {idToEdit} не существует");
+                            }
+
+                            break;
+                        }
+
+
+                    case Commands.DELETE:
+                        Console.Write("Введите id записи, которую желаете удалить: ");
+                        int idToDelete = Convert.ToInt32(Console.ReadLine());
+
+                        if (RemoveNote(idToDelete))
+                        {
+                            Console.WriteLine($"Запись с id {idToDelete} удалена");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Записи с id {idToDelete} не существует");
+                        }
+
+                        break;
+
+
+                    case Commands.FIND:
+                        {
+                            Console.Write("Введите id записи, которую желаете найти: ");
+                            int idToFind = Convert.ToInt32(Console.ReadLine());
+
+                            Person person = Find(idToFind);
+
+                            if (person != null)
+                            {
+                                Console.WriteLine(person);
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Записи id {idToFind} не существует");
+                            }
+
+                            break;
+                        }
+
+
+                    case Commands.PRINT:
+                        PrintAll();
+                        break;
+
+
+                    case Commands.INFO:
+                        ConsoleMenuInfo();
+                        break;
+
+
+                    case Commands.END:
+                        break;
+
+
+                    default:
+                        Console.WriteLine("Неверная команда");
+                        break;
+                }
+            } while (command != Commands.END);
+
+        }
+
+        static void Main(string[] args)
+        {
+            Notebook notebook = new Notebook();
+            notebook.ConsoleMenu();
+        }
+    }
+
+    public enum Commands
+    {
+        ERROR, // default value
+        ADD,
+        EDIT,
+        DELETE,
+        FIND,
+        PRINT,
+        INFO,
+        END
+    }
+}
---
+++ lab1/Person.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+
+        
+        public int PhoneNumber { get; set; }
+
+        public string Country { get; set; }
+
+        public string BirthDate { get; set; }
+
+        public string Organization { get; set; }
+
+        public string Position { get; set; }
+
+        public string OtherNotes { get; set; }
+
+
+        public void FillFields()
+        {
+            Console.Write("Фамилия: ");
+            Surname = ValidateRequiredFields(Console.ReadLine(), "Фамилия");
+
+            Console.Write("Имя: ");
+            Name = ValidateRequiredFields(Console.ReadLine(), "Имя");
+
+            Console.Write("Отчество (необязательно): ");
+            Patronymic = Console.ReadLine();
+
+            Console.Write("Номер телефона: ");
+            int phoneNumber;
+            while (!int.TryParse(Console.ReadLine(), out phoneNumber))
+            {
+                Console.WriteLine("Данное поле является обязательным для заполнения только цифрами");
+                Console.Write("Номер телефона: ");
+            }
+            PhoneNumber = phoneNumber;
+
+            Console.Write("Страна: ");
+            Country = ValidateRequiredFields(Console.ReadLine(), "Страна");
+
+            Console.Write("Дата рождения (необязательно): ");
+            BirthDate = Console.ReadLine();
+
+            Console.Write("Организация (необязательно): ");
+            Organization = Console.ReadLine();
+
+            Console.Write("Должность (необязательно): ");
+            Position = Console.ReadLine();
+
+            Console.Write("Заметки (необязательно): ");
+            OtherNotes = Console.ReadLine();
+        }
+
+        private string ValidateRequiredFields(string fieldValue, string fieldName)
+        {
+            while (string.IsNullOrEmpty(fieldValue))
+            {
+                Console.WriteLine("Данное поле является обязательным для заполнения");
+                Console.Write(fieldName + ": ");
+                fieldValue = Console.ReadLine();
+            }
+            return fieldValue;
+        }
+
+        public override string ToString()
+        {
+            return $"{Surname} {Name} {Patronymic}".Trim() + $" {PhoneNumber} " + (((($"{Country} {BirthDate}").Trim() + $" {Organization} ").Trim() + $" {Position} ").Trim() + $" {OtherNotes} ").Trim();
+        }
+
+    }
+}
